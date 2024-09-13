66bb1f8b62a2f9499a46bfc67eac05af6f080fbe
Luís Filipe Velasco da Silva <f.l.velasko@gmail.com>
2024-09-12 23:41:29 -0300

    Moving nvim OS id to proper file

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,17 +1,23 @@
-vim.g.allmodes = {"", "i", "v", "c", "o", "t", "n"}
+vim.g.allmodes = { "", "i", "v", "c", "o", "t", "n" }
+
+-- Getting OS
+local fd_os_release = assert(io.open("/etc/os-release"), "r")
+local s_os_release = fd_os_release:read("*a")
+fd_os_release:close()
+vim.g.system_distribution = s_os_release:lower()
 
 require('custom_keymaps')
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 
 vim.opt.rtp:prepend(lazypath)
