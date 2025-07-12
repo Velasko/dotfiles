fd62e249fb785f703a3e9c219489a083a82732fe
Luís Filipe Velasco da Silva <f.l.velasko@gmail.com>
2025-07-12 03:52:06 -0300

    Zsh: Theme fixing

--- dependencies/install.sh
+++ dependencies/install.sh
@@ -2,4 +2,8 @@ rpm-ostree install --idempotent --allow-inactive -yA $(<./main.txt)
 xargs go install <go.txt
 xargs cargo install <cargo.txt
 
+# This must be run for the theme to actually work.
+chmod +x /home/velasco/.config/base16-shell/scripts/base16-pop.sh
+
+# Setting user terminal to zsh
 chsh -s $(which zsh)
