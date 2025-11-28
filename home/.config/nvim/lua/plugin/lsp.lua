2b69173ad1eacd03b19f1a55081581c6f6c87052
Luís Filipe Velasco da Silva <f.l.velasko@gmail.com>
2025-11-28 19:31:39 -0300

    nvim: LSP update

--- home/.config/nvim/lua/plugin/lsp.lua
+++ home/.config/nvim/lua/plugin/lsp.lua
@@ -1,82 +1,24 @@
 return {
-	"VonHeikemen/lsp-zero.nvim",
-	dependencies = {
-		"neovim/nvim-lspconfig",
-		"williamboman/mason.nvim",
-		"williamboman/mason-lspconfig.nvim",
-	},
+	"neovim/nvim-lspconfig",
 	config = function()
-		local lsp = require('lsp-zero');
-
-		lsp.on_attach(function(_client, buffer)
-			lsp.default_keymaps({ buffer });
-		end);
-
-		local ensure_installed = {
-			-- lua
-			"lua_ls",
-
-			-- python
-			"ruff",
-			"jedi_language_server",
-
-			-- rust
-			"rust_analyzer",
-
-			-- Toml
-			"taplo",
-
-			-- yaml
-			"yamlls",
-		}
-
-		-- Mason ref config: https://github.com/williamboman/mason-lspconfig.nvim
-		require("mason").setup({ PATH = "append" });
-		require("mason-lspconfig").setup({
-			ensure_installed = ensure_installed,
-			handlers = {
-				function(server_name)
-					require("lspconfig")[server_name].setup({});
-				end,
-				["jedi_language_server"] = function()
-					require("lspconfig").jedi_language_server.setup({
-						single_file_support = false
-					})
-				end,
-				["yamlls"] = function()
-					require("lspconfig").yamlls.setup({
-						on_attach = function(client, bufnr)
-							client.server_capabilities.documentFormattingProvider = true
-						end,
-						settings = {
-							yaml = {
-								schemas = {
-									["https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/v1.18.0-standalone-strict/all.json"] =
-									"/*.k8s.yaml",
-									["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
-								},
-							},
-						},
-					})
-				end
-			},
-		});
-
-		local signs = { Error = "!", Warn = "", Hint = "", Info = "" }
-
-		for type, icon in pairs(signs) do
-			local hl = "DiagnosticSign" .. type
-			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-		end
-
-		vim.api.nvim_create_autocmd("BufWritePre", {
-			callback = function()
-				local bufnr = vim.lsp.buf.format()
-				local clients = vim.lsp.buf_get_clients(bufnr)
-				if next(clients) ~= nil then
-					vim.lsp.buf.format();
-				end
-			end
+		vim.diagnostic.config({
+			virtual_text = true,
+			underline = true,
+			signs = {
+				text = {
+					[vim.diagnostic.severity.ERROR] = "!",
+					[vim.diagnostic.severity.WARN] = "",
+					[vim.diagnostic.severity.INFO] = "󰙎",
+					[vim.diagnostic.severity.HINT] = "󰉀",
+				}
+			}
 		})
+
+		vim.api.nvim_create_autocmd("LspAttach", {
+			group = vim.api.nvim_create_augroup("Lspconfig", { clear = true }),
+			callback = function(args) end
+		})
+
+		vim.lsp.enable({ "rust_analyzer", "lua_ls", "taplo", "yamlls", "ruff", "jedi_language_server" })
 	end
 }
